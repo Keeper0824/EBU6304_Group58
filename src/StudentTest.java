95d8be593ac91072de68d3ac31eb7cbb87b1b95f
Keeper0824 <jp2022213462@qmul.ac.uk>
2025-03-27 13:38:50 +0800

    test

---
+++ src/StudentTest.java
@@ -0,0 +1,11 @@
+package src;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class StudentTest {
+
+    @org.junit.jupiter.api.Test
+    public void test() {
+        assertEquals(3,3);
+    }
+}
