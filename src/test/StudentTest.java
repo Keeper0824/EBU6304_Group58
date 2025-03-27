6ceafddadbf3ad4247a6424491430c997f572e73
Keeper0824 <jp2022213462@qmul.ac.uk>
2025-03-27 14:56:56 +0800

    test

--- src/test/StudentTest.java
+++ src/test/StudentTest.java
@@ -1,6 +1,6 @@
 package src.test;
 import src.main.Student;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -9,7 +9,7 @@ public class StudentTest {
     @org.junit.jupiter.api.Test
     public void testCreateStudent() {
         Student s1 = new Student();
-        s1.setName("Keeper_69");
+        s1.setName("Keeper");
         assertEquals("Keeper",s1.getName());
     }
 }
