5055f416551aba41eda8776048e71e5bf9aa0dc6
Keeper0824 <jp2022213462@qmul.ac.uk>
2025-03-27 14:02:46 +0800

    test

--- src/test/StudentTest.java
+++ src/test/StudentTest.java
@@ -7,7 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StudentTest {
 
     @org.junit.jupiter.api.Test
-    public void test() {
+    public void testCreateStudent() {
         Student s1 = new Student();
+        s1.getName();
     }
 }
