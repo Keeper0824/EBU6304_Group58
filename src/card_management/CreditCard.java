063bcb169fd3ba5cd77801a82f90899c45bbfa27
jmmboy <156645457+jmmboy@users.noreply.github.com>
2025-04-17 13:16:32 +0800

    Add files via upload

---
+++ src/card_management/CreditCard.java
@@ -0,0 +1,22 @@
+package card_management;
+
+
+public class CreditCard {
+    private final String cardNumber;
+    private final String cardHolder;
+    private final String expiryDate;
+    private final String cvv;
+
+    public CreditCard(String num, String holder, String date, String cvv) {
+        this.cardNumber = num;
+        this.cardHolder = holder;
+        this.expiryDate = date;
+        this.cvv = cvv;
+    }
+
+    // Getter方法需要与TableView的PropertyValueFactory对应
+    public String getCardNumber() { return cardNumber; }
+    public String getCardHolder() { return cardHolder; }
+    public String getExpiryDate() { return expiryDate; }
+    public String getCvv() { return cvv; }
+}
