e2aff0a9ad656191275848ff8177584a1d63d875
Keeper0824 <jp2022213462@qmul.ac.uk>
2025-04-19 21:31:31 +0800

    Modify some info like readme

--- transaction/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.transaction {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.transaction to javafx.fxml;
-    exports com.example.transaction;
-}
