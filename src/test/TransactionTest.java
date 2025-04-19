7d51061cc71e8163965368309dd1afcecb1513b1
Keeper0824 <jp2022213462@qmul.ac.uk>
2025-04-19 19:49:51 +0800

    Add story 15 Final

--- src/test/TransactionTest.java
+++ src/test/TransactionTest.java
@@ -1,11 +1,10 @@
-package src.test;
+package test;
 
 import org.junit.jupiter.api.Assertions;
 import src.main.java.CashFlowVisualization_story15.Transaction;
 
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 public class TransactionTest {
@@ -19,7 +18,3 @@ public class TransactionTest {
         Assertions.assertEquals(400, data.get(0).getAmount() - data.get(1).getAmount());
     }
 }
-
-
-
-
