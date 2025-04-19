ba1fc1439b1e49c548cbc3826d0a0df1b6587a03
Keeper0824 <jp2022213462@qmul.ac.uk>
2025-04-19 21:02:23 +0800

    Modify some info like readme

--- AdminDashboard/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.userstory4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.userstory4 to javafx.fxml;
-    exports com.example.userstory4;
-}
